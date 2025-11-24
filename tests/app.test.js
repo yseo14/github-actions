98bb5b1e84ff653ae83608e0e92129239639a470
yseo14 <lmslys14@naver.com>
2025-11-24 14:27:29 +0900

    Initial commit

---
+++ tests/app.test.js
@@ -0,0 +1,15 @@
+const request = require("supertest");
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello,  World!");
+});
+
+describe("GET /", () => {
+  it("should return Hello, World!", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual("Hello, World!");
+  });
+});
